0412aa021022691399a9cf1e5bf7179074b1ec1c
linhus <leajayaseelaa@gmail.com>
2021-11-15 15:32:03 +0100

    siste Igjen

--- scripts/quiz-test.js
+++ scripts/quiz-test.js
@@ -1,78 +1,71 @@
 //Oppretter en funksjon som kan ta imot array gjennom parameter arrayToUse
-function loadQuiz( arrayToUse ){
 
-    //Sletter evt resultat ved bytte av quiz
-    //savedResults.innerHTML = ""
-    resultsOutput.innerHTML = ""
-    //quizOutput2.innerHTML = ""
-    submitBtn.style.display = "block"
+let loadQuiz = (arrayToUse) => {
+  //Sletter evt resultat ved bytte av quiz
+  //savedResults.innerHTML = ""
+  resultsOutput.innerHTML = ""
+  //quizOutput2.innerHTML = ""
+  submitBtn.style.display = "block"
 
-    //Lagrer HTML output
-    const output = []
+  //Lagrer HTML output
+  const output = []
 
-    arrayToUse.forEach(activeQuestion, questionNo => {
-                //Lagrer mulige svar
-                const options = []
-        
-                 //og for hvert mulige svar..
-                for(choice in activeQuestion.options){
-         
-                    //..legg til radioKnapp i HTML
-                 options.push(`
+  arrayToUse.forEach((activeQuestion) => {
+    //Lagrer mulige svar
+    const options = []
+
+    //og for hvert mulige svar..
+    for (choice in activeQuestion.options) {
+      //..legg til radioKnapp i HTML
+      options.push(`
                  <label class="quiz-label">
-                         <input type="radio" name="questions${questionNo}" value="${choice}">
+                         <input type="radio" name="questions${activeQuestion.questionNo}" value="${activeQuestion.choice}">
                          ${choice} :
                          ${activeQuestion.options[choice]}<br>
                      </label>
-                    `
-                    )                 }
-             //push spørsmål og svaralternativ til HTML
-             output.push(`
+                    `)
+    }
+    //push spørsmål og svaralternativ til HTML
+    output.push(`
                  <div class="question"> ${activeQuestion.question}</div>
-                 <div class="options"> ${options.join('')}</div>
-                `
-            )
-        }
-    )
-    quizOutput.innerHTML = output.join('')
+                 <div class="options"> ${options.join("")}</div>
+                `)
+  })
+  quizOutput.innerHTML = output.join("")
 }
 
+function displayResults() {
+  resultsOutput.innerHTML = ""
+  submitBtn.style.display = "block"
 
+  //Hent svaralternativ fra quizen
+  const optionsBoxes = quizOutput.querySelectorAll(".options")
+  //Brukersvar
+  let noOfRightAnswers = 0
 
-function displayResults(){
+  //For each question..
+  arrayToUse.forEach((activeQuestion) => {
+    //Finn det riktige svaret
+    const optionsBox = optionsBoxes[activeQuestion.questionNo]
+    const userChoice = `input[name=questions${activeQuestion.questionNo}]:checked`
+    const userAnswer = (optionsBox.querySelector(userChoice) || {}).value
 
-    resultsOutput.innerHTML = "";
-    
-    //Hent svaralternativ fra quizen
-    const optionsBoxes = quizOutput.querySelectorAll(".options")
-    //Brukersvar
-    let noOfRightAnswers = 0
+    //hvis svaret er riktig
+    if (userAnswer === activeQuestion.rightAnswer) {
+      //legg til i antall riktige svar
+      noOfRightAnswers++
+      localStorage.setItem("result", noOfRightAnswers)
+      //localStorageValue = `Ditt resultat ${noOfRightAnswers} er nå lagret`
 
+      optionsBoxes[questionNo].style.color = "lightgreen"
+    } else {
+      optionsBoxes[questionNo].style.color = "red"
+    }
+  })
+  localStorage.setItem("result", noOfRightAnswers)
+  //savedResults.innerHTML = localStorageValue;
 
-
-    //For each question..
-    arrayToUse.forEach( (activeQuestion, questionNo) => {
-        //Finn det riktige svaret
-        const optionsBox = optionsBoxes[questionNo]
-        const userChoice = `input[name=questions${questionNo}]:checked`
-        const userAnswer = (optionsBox.querySelector(userChoice) || {}).value;
-
-        //hvis svaret er riktig
-        if(userAnswer === activeQuestion.rightAnswer){
-            //legg til i antall riktige svar
-            noOfRightAnswers++
-            localStorage.setItem("result", noOfRightAnswers)
-            //localStorageValue = `Ditt resultat ${noOfRightAnswers} er nå lagret`
-
-            optionsBoxes[questionNo].style.color = "lightgreen"
-        }else{
-            optionsBoxes[questionNo].style.color = "red"
-        }
-    })
-    localStorage.setItem("result", noOfRightAnswers)
-    //savedResults.innerHTML = localStorageValue;
-
-    resultsOutput.innerHTML = `${noOfRightAnswers} av ${arrayToUse.length}`
+  resultsOutput.innerHTML = `${noOfRightAnswers} av ${arrayToUse.length}`
 }
 
 /* VARIABLER */
@@ -81,34 +74,31 @@ let resultsOutput = document.getElementById("results-div")
 const planetBtn = document.querySelector(".planet-icon")
 const submitBtn = document.getElementById("submit-btn")
 
-
-
 const savedResults = document.querySelector(".saved-results")
 
 /* ARRAY */
 let planetArray = [
-    {
-        question: "Hva er kallenavnet til Jorda?",
-        options: {
-                a: "Den Blå Lagune",
-                b: "Den Blå Planeten",
-                c: "Venus"
-        },
-        rightAnswer: "b"
-        
+  {
+    question: "Hva er kallenavnet til Jorda?",
+    options: {
+      a: "Den Blå Lagune",
+      b: "Den Blå Planeten",
+      c: "Venus",
     },
-    {
-        question: "Hvorfor blir Mars kalt Den Røde Planeten?",
-        options: {
-            a: "Fordi den består av flytende lava",
-            b: "Fordi den er så nærme Sola",
-            c: "Fordi overflaten er dekket av jernoksid"
-        },
-        rightAnswer: "c"
-    }
+    rightAnswer: "b",
+  },
+  {
+    question: "Hvorfor blir Mars kalt Den Røde Planeten?",
+    options: {
+      a: "Fordi den består av flytende lava",
+      b: "Fordi den er så nærme Sola",
+      c: "Fordi overflaten er dekket av jernoksid",
+    },
+    rightAnswer: "c",
+  },
 ]
 
 planetBtn.addEventListener("click", loadQuiz(planetArray))
 submitBtn.addEventListener("click", displayResults, () => {
-    savedResults = ""
+  savedResults = ""
 })
