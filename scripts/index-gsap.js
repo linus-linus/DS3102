1aa4b41ffcc38f454e204afb7c895bfd4385802b
linhus <leajayaseelaa@gmail.com>
2021-11-15 17:46:32 +0100

    updait

--- scripts/index-gsap.js
+++ scripts/index-gsap.js
@@ -4,9 +4,16 @@ gsap.from(".logo-container__logo-img", {
 })
 
 gsap.from(".start-section__initializer", {
+<<<<<<< Updated upstream
     delay: 4,
     duration: 4,
     y: 80,
     opacity: 0
+=======
+  duration: 4,
+  y: 80,
+  opacity: 0,
+  delay: 3,
+>>>>>>> Stashed changes
 })
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,54 @@
 /* Add name to localStorage */
 
 // HTML-elements
-const storeNameBtn = document.querySelector(".add-name__confirm-name-btn")
+
+let storeNameBtn = document.querySelector(".add-name__confirm-name-btn")
 const nameInput = document.querySelector(".add-name__name-input")
 const nameOutput = document.querySelector(".choose-action__name-output")
 
 // Function that stores the input value to localStorage, and also returns the value to innerHTML
-function storeName(){
-    localStorage.setItem("username", nameInput.value)
-    let userName = localStorage.getItem("username")
-    nameOutput.innerHTML = `
+/*
+let writeName = () => {
+  let inputName = yourinputname.value
+
+  if (nameInput.value === inputName) {
+    let storeNameBtn = (document.querySelector(
+      ".add-name__confirm-name-btn"
+    ).style.display = "block")
+  }
+}
+
+yourinputname.addEventListener("input", writeName)
+*/
+
+/*trykke på start kanpp, for å komme til neste*/
+const startBtn = document.querySelector(".start-btn")
+
+let startLearning = () => {
+  let addNameSection = (document.querySelector(".add-name").style.display =
+    "block")
+}
+
+startBtn.addEventListener("click", startLearning)
+
+/* input felt --> grønn ok knapp  */
+let textValue = () => {
+  let storeNameBtn = (document.querySelector(
+    ".add-name__confirm-name-btn"
+  ).style.display = "block")
+}
+
+nameInput.addEventListener("input", textValue)
+
+function storeName() {
+  localStorage.setItem("username", nameInput.value)
+  let userName = localStorage.getItem("username")
+  nameOutput.innerHTML = `
     <p>Hei ${userName}! Hva har du lyst til å gjøre?</p>
     `
-    // re-using this function to display new section and remove previous one
-    addNameSection.style.display = "none"
-    chooseActionSection.style.display = "block"
+  // re-using this function to display new section and remove previous one
+  addNameSection.style.display = "none"
+  chooseActionSection.style.display = "block"
 }
 
 storeNameBtn.addEventListener("click", storeName)
@@ -28,12 +62,9 @@ let addNameSection = document.querySelector(".add-name")
 let chooseActionSection = document.querySelector(".choose-action")
 let initiateVisibility = document.querySelector(".start-section__initializer")
 
-function makeVisible(){
-    
-    addNameSection.style.display = "block"
-    startSection.style.display = "none"
+function makeVisible() {
+  addNameSection.style.display = "block"
+  startSection.style.display = "none"
 }
 
 initiateVisibility.addEventListener("click", makeVisible)
-
-
